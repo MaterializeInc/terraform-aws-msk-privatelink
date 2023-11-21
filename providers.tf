a3000bcaedc8f88e0180f668a58cadde8de9f513
Dennis Webb <dhwebb@gmail.com>
2023-11-21 11:03:10 -0600

    allows module to be used with for_each
    Module is considered legacy and will not allow calling Terraform to use `for_each` on the module. Removing the AWS provider section from providers allows this to work.

--- providers.tf
+++ providers.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 4.0, < 6.0"
     }
   }
-}
-
-provider "aws" {
-  region = var.aws_region
 }
